f7469eb865038486998ab9074646a83de98f5690
Kellinton <kellintonbrasil@gmail.com>
2023-12-04 17:17:14 -0300

    Challenge 22

---
+++ challenges/level0/the-farm-problem.js
@@ -0,0 +1,33 @@
+// challenge  22
+
+
+
+function contarAnimais(galinhas, vacas, porcos) {
+
+    return (galinhas * 2) + (vacas * 4) + (porcos * 4);
+
+}
+
+console.log(contarAnimais(1, 2, 3));
+
+
+
+
+
+
+
+
+
+
+
+// Neste desafio, um fazendeiro está pedindo que você 
+// lhe diga quantas pernas podem ser contadas entre 
+// todos os seus animais. O agricultor cria três espécies:
+
+// galinhas = 2 patas
+// vacas = 4 patas
+// porcos = 4 patas
+
+// O fazendeiro contou seus animais e ele lhe dá um subtotal
+// para cada espécie. Você tem que implementar uma função que 
+// retorna o número total de pernas de todos os animais.
