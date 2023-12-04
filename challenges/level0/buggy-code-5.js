4c5e51a77abed607d1e2c0e13c5ea86e8a8aa1f6
Kellinton <kellintonbrasil@gmail.com>
2023-12-04 17:00:06 -0300

    challenge 20

---
+++ challenges/level0/buggy-code-5.js
@@ -0,0 +1,15 @@
+// challenge 20
+
+function mostrarArray(numero) {
+
+    let novoArray = [];
+
+    for (let index = 1; index <= numero; index++) {
+        novoArray.push(index);
+        
+    }
+
+    return novoArray;
+}
+
+console.log(mostrarArray(7));
