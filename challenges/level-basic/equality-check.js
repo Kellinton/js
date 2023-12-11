35f52801cfc762eccecd83c0283e5bf4dcb64a01
Kellinton <kellintonbrasil@gmail.com>
2023-12-11 18:20:18 -0300

    função que verifica a igualdade de dois valores dados os parametros a & b

--- challenges/level-basic/equality-check.js
+++ challenges/level-basic/equality-check.js
@@ -1,4 +1,15 @@
 // challenge 27
 
-// function 
+// função que verifica a igualdade de dois valores 
+// diferentes dados os parâmetros a e b.
 
+
+function checarIgualdade(a, b){
+    if(a === b){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(checarIgualdade(1, 1));
