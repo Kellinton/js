1c047745765821aa7e180f82271bb8f9fc500e5b
Kellinton <kellintonbrasil@gmail.com>
2023-12-16 14:32:28 -0300

    função que recebe  um valor boolean e a retorne como uma string

---
+++ challenges/level-basic/boolean-to-string-conversion.js
@@ -0,0 +1,13 @@
+/*
+Crie uma função que receba um valor
+boolean e a retorne como uma string
+*/
+
+
+function converterParaString(boolean){
+    return 'True';
+}
+
+let resultado = converterParaString(true);
+
+console.log(resultado);
