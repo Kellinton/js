1eef19c5e2675927c73d7dba20911c75e1f4c876
Kellinton <kellintonbrasil@gmail.com>
2023-12-18 10:32:05 -0300

    feature: adicionado uma class e o método construtor

--- challenges/level-intermediate/POO-calculating-IMC.js
+++ challenges/level-intermediate/POO-calculating-IMC.js
@@ -6,57 +6,52 @@
 */
 
 class Pessoa {
-    nome;
-    altura;
-    peso;
+   nome;
+   peso;
+   altura;
 
-    calcularIMC() {
-        return this.peso / (Math.pow(this.altura, 2));
-    }
+   constructor(nome, peso, altura) {
+       this.nome = nome;
+       this.peso = peso;
+       this.altura = altura;
+   }
 
-    classificarIMC() {
-        const calculoIMC = this.calcularIMC();
+   calcularIMC(){
 
-        if(calculoIMC < 18.5){
+       return this.peso / Math.pow(this.altura, 2);
+   }
 
-            return `\n ${this.nome}, seu IMC é: ${calculoIMC.toFixed(2)}. \n\n    Você está Abaixo do peso!`
-         
-         }else if(calculoIMC >= 18.5 && calculoIMC <= 25){
-     
-            return `\n ${this.nome}, seu IMC é: ${calculoIMC.toFixed(2)}. \n\n    Você está com Peso normal!`
-         
-         }else if(calculoIMC >= 25 && calculoIMC <= 30){
-         
-            return `\n ${this.nome}, seu IMC é: ${calculoIMC.toFixed(2)}. \n\n    Você está Acima do peso!`
-         
-         }else if(calculoIMC >= 30 && calculoIMC <= 40){
-         
-            return `\n ${this.nome}, seu IMC é: ${calculoIMC.toFixed(2)}. \n\n    Você está Obeso!`
-         
-         }else{
-         
-            return `\n ${this.nome}, seu IMC é: ${calculoIMC.toFixed(2)}. \n\n    Você está com Obesidade grave!`
-         
-         }
-    }
+   classificarIMC() {
+
+       if(this.calcularIMC() < 18.5){
+
+           return `IMC: ${this.calcularIMC().toFixed(2)}, está Abaixo do peso!`
+        
+        }else if(this.calcularIMC() >= 18.5 && this.calcularIMC() <= 25){
+    
+           return `IMC: ${this.calcularIMC().toFixed(2)}, está com Peso normal!`
+        
+        }else if(this.calcularIMC() >= 25 && this.calcularIMC() <= 30){
+        
+           return `IMC: ${this.calcularIMC().toFixed(2)}, está Acima do peso!`
+        
+        }else if(this.calcularIMC() >= 30 && this.calcularIMC() <= 40){
+        
+           return `IMC: ${this.calcularIMC().toFixed(2)}, está Obeso!`
+        
+        }else{
+        
+           return `IMC: ${this.calcularIMC().toFixed(2)}, está com Obesidade grave!`
+        
+        }
+   }
 
 }
 
-const Luffy = new Pessoa();
-Luffy.nome = 'Luffy';
-Luffy.altura = 1.80;
-Luffy.peso = 120;
-
-const Ace = new Pessoa();
-Ace.nome = 'Ace';
-Ace.altura = 1.80;
-Ace.peso = 60;
+const luffy = new Pessoa('Luffy', 70, 1.75);
+const ace = new Pessoa('Ace', 70, 1.75);
 
 
-console.log(Luffy.classificarIMC());
-console.log(Ace.classificarIMC());
-
-
-
-
+console.log(luffy.classificarIMC());
+console.log(ace.classificarIMC());
 
