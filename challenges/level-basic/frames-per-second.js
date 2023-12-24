351555f709ceab44ff45b7bcdc1c8d2775593cc3
Kellinton <kellintonbrasil@gmail.com>
2023-12-24 16:18:27 -0300

    função que retorna o número de frames mostrado em um determinado número e minutos para um determinado FPS.

---
+++ challenges/level-basic/frames-per-second.js
@@ -0,0 +1,15 @@
+
+// Create a function that returns the number of frames
+//  shown in a given number
+//  of minutes for a certain FPS.
+
+function frames(minutes, fps) {
+
+    return minutes * fps * 60;
+
+}
+
+frames(1, 1);
+frames(10, 1);
+frames(10, 25);
+
