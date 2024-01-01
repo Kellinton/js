f22e355c39854e1526d7da96c97012a776570642
Kellinton <kellintonbrasil@gmail.com>
2024-01-01 17:32:57 -0300

    função que recebe um array e uma string como argumentos e retorna o índice da string.

---
+++ challenges/level-basic/find-the-index.js
@@ -0,0 +1,12 @@
+
+function teste(array, string) {
+    return array.indexOf(string);
+}
+
+const resposta = teste(["Red", "blue", "Blue", "Green"], "Green");
+
+console.log(resposta);
+
+
+// função que recebe um array e uma string como argumentos e retorna o índice da string.
+
