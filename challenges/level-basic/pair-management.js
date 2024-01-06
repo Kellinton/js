38f386f8f8fc481d1141d143205b5b248c148537
Kellinton <kellintonbrasil@gmail.com>
2024-01-06 16:53:22 -0300

    retornar array que contém quatro argumentos

---
+++ challenges/level-basic/pair-management.js
@@ -0,0 +1,15 @@
+
+function retornarArray(n1, n2, n3, n4) {
+    let array = [];
+
+    array.push(n1, n2, n3, n4);   
+    return array;
+
+}
+
+const resultado = retornarArray(10, 2, 5, 7);
+console.log(resultado);
+
+
+
+// Given two arguments, return an array which contains these two arguments.
