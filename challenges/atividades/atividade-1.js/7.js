5b8d4ee725ab2bf6ba05533e626047a9d7edf0fc
Kellinton <kellintonbrasil@gmail.com>
2024-01-10 19:54:48 -0300

    Calcular média de quatro números/notas

---
+++ challenges/atividades/atividade-1.js/7.js
@@ -0,0 +1,32 @@
+// Calcular média de quatro números/notas 
+
+class Aluno {
+    nome;
+    notas;
+
+    constructor(nome, notas) {
+        this.nome = nome;
+        this.notas = notas;
+    }
+    
+    calcularMedia(){
+        let soma = 0;
+        let media = 0;
+
+        for (let i = 0; i < this.notas.length; i++) {
+
+            soma += this.notas[i];
+
+        }
+        media = (soma / this.notas.length);
+
+        return `A média de ${this.nome} é: ${media}`;
+    }
+}
+
+function main() {
+    const Fulano = new Aluno("Fulano", [5, 8, 10, 10]);
+    console.log(Fulano.calcularMedia());
+}
+
+main();
