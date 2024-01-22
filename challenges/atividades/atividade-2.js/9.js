0ceae65bda77bd8455def8611b45229f168ca99a
Kellinton <kellintonbrasil@gmail.com>
2024-01-22 09:16:39 -0300

    up

---
+++ challenges/atividades/atividade-2.js/9.js
@@ -0,0 +1,19 @@
+/*
+
+Pedro comprou um saco de ração com peso em quilos. Pedro possui 2 (dois) gatos
+para os quais fornece a quantidade de ração em gramas. Faça um algortimo que receba
+o peso do saco de ração e a quantidade de ração fornecida para cada gato.
+Calcule e mostre quanto restará de ração no saco após 5 (cinco) dias.
+
+*/
+
+let sacoRacao = 10; // kilos
+let gatoUm = 50; // gramas
+let gatoDois = 40; // gramas
+
+
+function calcularRacao(sacoRacao, gatoUm, gatoDois) {
+    
+}
+
+const resultado = calcularRacao
