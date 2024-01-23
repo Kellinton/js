978bfe14904ed15d450c5ebc4eb311d95f39c841
Kellinton <kellintonbrasil@gmail.com>
2024-01-23 09:05:17 -0300

    up

--- challenges/atividades/atividade-2.js/9.js
+++ challenges/atividades/atividade-2.js/9.js
@@ -3,7 +3,7 @@
 Pedro comprou um saco de ração com peso em quilos. Pedro possui 2 (dois) gatos
 para os quais fornece a quantidade de ração em gramas. Faça um algortimo que receba
 o peso do saco de ração e a quantidade de ração fornecida para cada gato.
-Calcule e mostre quanto restará de ração no saco após 5 (cinco) dias.
+Calcule e mostre quanto restará de ração no saco após 5 (cinco) dias
 
 */
 
