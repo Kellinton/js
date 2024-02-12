788569a99e62e00ca710fbb9e97d4b67e9fd7508
Kellinton <kellintonbrasil@gmail.com>
2024-02-12 15:34:49 -0300

    Método Array() e alias[]

--- challenges/level-intermediate/arrayTwoForms.js
+++ challenges/level-intermediate/arrayTwoForms.js
@@ -1,13 +1,15 @@
 // Formas de declarar uma variável como array
 
 const listaFrutas = Array(); // utilizando método array
+// const listaFrutas = Array('Maça', 'Banana');
 
-listaFrutas['Vermelha'] = 'Maça'; // o índice/chave pode ser qualquer coisa (geralmente é número)
+listaFrutas['Vermelha'] = 'Maça'; // o índice/chave pode ser qualquer coisa (Mas se for usar o método length, a ordem precisa ser númerica e bem definida. 0...)
 listaFrutas['Amarela'] = 'Banana';
 
 console.log(listaFrutas['Amarela'] + ', ' + listaFrutas['Vermelha']);
 
 const listaFruta = []; // [] é um alias/apelido para o método Array()
+// const listaFruta = ['Maça', 'Banana'];
 
 listaFruta[0] = 'Maça';
 listaFruta[1] = 'Banana';
