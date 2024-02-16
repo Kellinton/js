41475132d5b5aa475c146a6cb54ee44ca586c35f
Kellinton <kellintonbrasil@gmail.com>
2024-02-16 16:51:44 -0300

    Escreva um programa que leia um número inteiro. Se o número lido for positivo, escreva uma mensagem indicando se ele é par ou ímpar. Se o número for negativo, escreva a seguinte mensagem “Este número não é positivo”.

---
+++ challenges/atividades/atividade-3.js/12.js
@@ -0,0 +1,13 @@
+// Escreva um programa que leia um número inteiro. Se o número lido for positivo, escreva uma mensagem indicando se ele é par ou ímpar. Se o número for negativo, escreva a seguinte mensagem “Este número não é positivo”.
+
+const numero = -10;
+
+function verificarNumero(numero) {
+    if(Math.sign(numero) == 1){
+        return 
+    }else{
+        
+    }
+}
+
+console.log(Math.sign(numero));
